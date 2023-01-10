de25d5efa6bf704e35a391d9d494ac3ab0d377a0
Lucian <la@checkmate.dk>
2023-01-10 12:51:58 +0100

    changing video on single project page

--- components/partials/single-project/SingleProject.vue
+++ components/partials/single-project/SingleProject.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="w-screen bg-secondary">
         <div class="h-fit flex items-center justify-center">
-            <video autoplay controls class="aspect-video object-cover max-h-screen w-full z-0" src="../../../assets/images/royal.mp4" alt="video"></video>
+            <video autoplay controls class="aspect-video object-cover max-h-screen w-full z-0" src="../../../assets/images/bikini.mp4" alt="video"></video>
         </div>
         <content-wrapper>
             <div class="py-10 border-b-2 border-fuchsia-400">
                 <div class="flex justify-between w-full px-5 sm:px-20">
                 <div class="text-center">
                     <h6 class=" pb-3">client</h6>
-                    <h3 class=" text-base md:text-3xl">Mærsk</h3>
+                    <h3 class=" text-base md:text-3xl">Underwear</h3>
                 </div>
                 <div class="text-center">
                     <h6 class=" pb-3">title</h6>
